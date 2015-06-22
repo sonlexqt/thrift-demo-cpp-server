d9f533d487471635ee8d2fac2af8f568ea536e37
thaisonle94 <thaisonle94@gmail.com>
2015-06-22 15:32:46 +0700

    Add initDb function to interact with mySQL database

--- CppServer.cpp
+++ CppServer.cpp
@@ -18,6 +18,10 @@
 #include "Poco/SplitterChannel.h"
 #include "Poco/PatternFormatter.h"
 #include "Poco/FormattingChannel.h"
+#include "Poco/Data/Session.h"
+#include <Poco/Data/MySQL/MySQLException.h>
+#include <Poco/Data/MySQL/Connector.h>
+#include <Poco/Data/SessionFactory.h>
 // include other libraries
 #include <iostream>
 #include <stdexcept>
@@ -25,6 +29,8 @@
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
+#include <sstream> 
+#include <string>
 
 /*
  * USING
@@ -45,6 +51,12 @@ using Poco::ConsoleChannel;
 using Poco::SplitterChannel;
 using Poco::PatternFormatter;
 using Poco::FormattingChannel;
+using namespace Poco::Data;
+using namespace Poco::Data::Keywords;
+using Poco::Data::Session;
+using Poco::Data::Statement;
+using Poco::Data::MySQL::ConnectionException;
+using Poco::Data::MySQL::StatementException;
 // using other namespaces
 using namespace std;
 using boost::shared_ptr;
@@ -53,6 +65,19 @@ using boost::shared_ptr;
  * GLOBAL VARS
  */
 int port = 0;
+std::string mySQLHost = "";
+int mySQLPort = 0;
+std::string mySQLDb = "";
+std::string mySQLUsername = "";
+std::string mySQLPassword = "";
+
+
+
+struct ViewCountInfo
+{
+    std::string username;
+    int         counter;
+};
 
 class APIsHandler : virtual public APIsIf {
 public:
@@ -62,25 +87,26 @@ public:
     }
 
     void put(const std::string& _username, const int32_t _newValue) {
-        Logger::root().information("put");
+        Logger::root().information("handle PUT request");
+        
     }
 
     void increase(const std::string& _username) {
-        Logger::root().information("increase");
+        Logger::root().information("handle INCREASE request");
     }
 
     int32_t get(const std::string& _username) {
-        Logger::root().information("get");
+        Logger::root().information("handle GET request");
     }
 
     bool ping() {
-        Logger::root().information("ping");
+        Logger::root().information("handle PING request");
         return true;
     }
 
 };
 
-void printPropertyFileInfo() {
+void getPropertiesInfo() {
     AutoPtr<PropertyFileConfiguration> pConf;
     pConf = new PropertyFileConfiguration("viewcount.properties");
     cout << "=== The following are information get from the viewcount.properties file: ===" << endl;
@@ -94,6 +120,23 @@ void printPropertyFileInfo() {
     
     string authorName = pConf->getString("AUTHOR_NAME");
     cout << "authorName: " << authorName << endl;
+    
+    mySQLHost = pConf->getString("MYSQL_HOST");
+    cout << "mySQLHost: " << mySQLHost << endl;
+    
+    mySQLPort = pConf->getInt("MYSQL_PORT");
+    cout << "mySQLPort: " << mySQLPort << endl;
+    
+    mySQLDb = pConf->getString("MYSQL_DB");
+    cout << "mySQLDb: " << mySQLDb << endl;
+    
+    mySQLUsername = pConf->getString("MYSQL_USERNAME");
+    cout << "mySQLUsername: " << mySQLUsername << endl;
+    
+    mySQLPassword = pConf->getString("MYSQL_PASSWORD");
+    cout << "mySQLPassword: " << mySQLPassword << endl;
+    
+    
 }
 
 void initLogger() {
@@ -115,11 +158,41 @@ void initLogger() {
     Logger::root().setChannel(pSplitter);
 }
 
+std::string convertToString(int number){
+    ostringstream convert;   // stream used for the conversion
+    convert << number;      // insert the textual representation of 'Number' in the characters in the stream
+    std::string res = convert.str(); // set 'Result' to the contents of the stream
+    return res;
+}
+
+void initDb(){
+    // register MySQL connector
+    Poco::Data::MySQL::Connector::registerConnector();
+    // create a session
+    std::string sessionStr = "host=";
+    sessionStr += mySQLHost;        
+    sessionStr += ";port=";
+    sessionStr += convertToString(mySQLPort);
+    sessionStr += ";db=";
+    sessionStr += mySQLDb;
+    sessionStr += ";user=";
+    sessionStr += mySQLUsername;
+    sessionStr += ";password=";
+    sessionStr += mySQLPassword;
+    sessionStr += ";compress=true;auto-reconnect=true";
+    Session session("MySQL", sessionStr);
+    // drop sample table, if it exists
+    session << "DROP TABLE IF EXISTS view_count_info", now;
+    // (re)create table
+    session << "CREATE TABLE view_count_info (username VARCHAR(30), counter INTEGER(5))", now;
+}
 int main(int argc, char **argv) {
     // print out viewcount.properties file information
-    printPropertyFileInfo();
+    getPropertiesInfo();
     // init POCO's logger settings
     initLogger();
+    // initialize the system database
+    initDb();
     
     // start the server
     shared_ptr<APIsHandler> handler(new APIsHandler());
